3407838375c4ae053484dcc020a447e7cda99dcd
T.J. Mazeika <t@mazeika.me>
2015-12-28 15:07:25 -0500

    Add PuncherReady

--- common/protocol.go
+++ common/protocol.go
@@ -10,8 +10,9 @@ type ProtocolMessage byte
 const (
     PuncherPing        ProtocolMessage = 0x00
     PuncherPong        ProtocolMessage = 0x01
-    DownloadClientType ProtocolMessage = 0x02
-    UploadClientType   ProtocolMessage = 0x03
-    ChecksumMismatch   ProtocolMessage = 0x04
-    ChecksumMatch      ProtocolMessage = 0x05
+    PuncherReady       ProtocolMessage = 0x02
+    DownloadClientType ProtocolMessage = 0x03
+    UploadClientType   ProtocolMessage = 0x04
+    ChecksumMismatch   ProtocolMessage = 0x05
+    ChecksumMatch      ProtocolMessage = 0x06
 )
