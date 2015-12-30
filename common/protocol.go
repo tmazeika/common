f56aa5ed933780e0060d457b56a82d582cd812e9
T.J. Mazeika <t@mazeika.me>
2015-12-30 15:33:41 -0500

    Add MessageChannel

--- common/protocol.go
+++ common/protocol.go
@@ -1,4 +1,9 @@
 package common
+import (
+    "os"
+    "fmt"
+    "net"
+)
 
 const (
     // UidLength is the length of the UID that the puncher server issues.
@@ -18,6 +23,49 @@ const (
     ChecksumMismatch   ProtocolMessage = 0x07
     ChecksumMatch      ProtocolMessage = 0x08
 )
+
+type Message struct {
+    desc ProtocolMessage
+    body []byte
+}
+
+func MessageChannel(conn net.Conn) (ch chan Message) {
+    ch = make(chan Message)
+
+    go func() {
+        for {
+            descBuff := make([]byte, 1)
+
+            if _, err := conn.Read(descBuff); err != nil {
+                fmt.Fprintf(os.Stderr, "Read error for '%s': %s", conn.RemoteAddr(), err)
+                return
+            }
+
+            desc := ProtocolMessage(descBuff[0])
+            lenBuff := make([]byte, 1)
+
+            if _, err := conn.Read(lenBuff); err != nil {
+                fmt.Fprintf(os.Stderr, "Read error for '%s': %s", conn.RemoteAddr(), err)
+                return
+            }
+
+            len := uint8(lenBuff[0])
+            bodyBuff := make([]byte, len)
+
+            if _, err := conn.Read(bodyBuff); err != nil {
+                fmt.Fprintf(os.Stderr, "Read error for '%s': %s", conn.RemoteAddr(), err)
+                return
+            }
+
+            ch <- Message{
+                desc: desc,
+                body: bodyBuff,
+            }
+        }
+    }()
+
+    return
+}
 
 func Mtob(msg ProtocolMessage) []byte {
     return []byte{byte(msg)}
