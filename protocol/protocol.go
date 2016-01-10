0e9e6388a78d9e4074764c80f7e556185e3c0ab2
T.J. Mazeika <t@mazeika.me>
2016-01-10 01:38:38 -0500

    Add ping/pong signals and SourceReady struct

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,6 +1,9 @@
 package protocol
 
-import "crypto/sha256"
+import (
+    "crypto/sha256"
+    "time"
+)
 
 type ClientType int
 type Signal     int
@@ -14,8 +17,22 @@ const (
 // Signals.
 const (
     ExitSignal Signal = iota
+
+    // PingSignal is sent from the puncher to a client, expecting a pong in
+    // response. The purpose of this is to measure the latency between the
+    // endpoints.
+    PingSignal
+
+    // PongSignal is sent from a client to the puncher.
+    PongSignal
+
     OkaySignal
 )
+
+type SourceReady struct {
+    addr   string
+    connAt time.Time
+}
 
 type FileInfoMessage struct {
     Name string
