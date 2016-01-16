34a1f6f45e4196f74c263e799ddb3e5fd34c8ae1
T.J. Mazeika <t@mazeika.me>
2016-01-16 17:03:29 -0500

    Simplify protocol

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,41 +1,15 @@
 package protocol
 
-import (
-	"crypto/sha256"
-	"time"
-)
+type NodeType int
 
-type ClientType int
-type Signal int
-
-// Client types.
+// NodeType enum.
 const (
-	TargetClient ClientType = iota
-	SourceClient
+	TargetNode = 0
+	SourceNode = 1
 )
 
-// Signals.
-const (
-	ExitSignal Signal = iota
-
-	// PingSignal is sent from the puncher to a client, expecting a pong in
-	// response. The purpose of this is to measure the latency between the
-	// endpoints.
-	PingSignal
-
-	// PongSignal is sent from a client to the puncher.
-	PongSignal
-
-	OkaySignal
-)
-
-type SourceReady struct {
-	addr   string
-	connAt time.Time
-}
-
-type FileInfoMessage struct {
+type FileInfo struct {
 	Name string
-	Size int64
-	Hash [sha256.Size]byte
+	Size uint64
+	Hash []byte
 }
