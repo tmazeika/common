537037d76645a6a995d30dec84b53b69f0e8ccf9
T.J. Mazeika <t@mazeika.me>
2016-01-16 19:11:46 -0500

    Exit is now an error

--- protocol/inbound.go
+++ protocol/inbound.go
@@ -2,16 +2,15 @@ package protocol
 
 import (
 	"encoding/gob"
+	"errors"
 	"fmt"
 )
 
-func Inbound(dec *gob.Decoder) (exit <-chan struct{}, msg <-chan struct{}, err <-chan error) {
-	exitCh := make(chan struct{})
+func Inbound(dec *gob.Decoder) (msg <-chan struct{}, err <-chan error) {
 	msgCh := make(chan struct{})
 	errCh := make(chan error)
 
 	go func() {
-		defer close(exitCh)
 		defer close(msgCh)
 		defer close(errCh)
 
@@ -25,11 +24,11 @@ func Inbound(dec *gob.Decoder) (exit <-chan struct{}, msg <-chan struct{}, err <
 		case MessageInbound:
 			msgCh <- struct{}{}
 		case ExitInbound:
-			exitCh <- struct{}{}
+			errCh <- errors.New("exiting")
 		default:
 			errCh <- fmt.Errorf("unknown InboundType 0x%x", inType)
 		}
 	}()
 
-	return exitCh, msgCh, errCh
+	return msgCh, errCh
 }
